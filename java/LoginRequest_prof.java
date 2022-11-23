393bfac6861df38625379e1354c0049fd8b81a2d
eoowkkx <106503022+eoowkkx@users.noreply.github.com>
2022-11-23 10:58:03 +0900

    Create LoginRequest_prof.java

---
+++ java/LoginRequest_prof.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginRequest extends StringRequest {
+
+    final static private String URL = "https://chlwogh0829.cafe24.com/UserLogin.php";
+    private Map<String, String> parameters;
+
+    public LoginRequest(String userID, String userPassword, Response.Listener<String> listener) {
+        super(Method.POST, URL, listener, null);
+        parameters = new HashMap<>();
+        parameters.put("userID", userID);
+        parameters.put("userPassword", userPassword);
+
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return parameters;
+    }
+
+
+}
