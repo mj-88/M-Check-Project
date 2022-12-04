cda70b658e4625ffc4bfac1006e3e6b1ef96828c
hoonani97 <103723848+hoonani97@users.noreply.github.com>
2022-12-04 13:35:40 +0900

    Update LoginRequest_stu.java

--- xml/LoginRequest_stu.java
+++ xml/LoginRequest_stu.java
@@ -1,8 +1,4 @@
-package com.example.myapplication;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
+package com.example.mcheck_student;
 
 import com.android.volley.Response;
 import com.android.volley.toolbox.StringRequest;
@@ -10,16 +6,16 @@ import com.android.volley.toolbox.StringRequest;
 import java.util.HashMap;
 import java.util.Map;
 
-public class LoginRequest_stu extends StringRequest {
+public class LoginRequest extends StringRequest {
 
-    final static private String URL = "https://chlwogh0829.cafe24.com/UserLogin_stu.php";
+    final static private String URL = "https://chlwogh0829.cafe24.com/StudentLogin.php";
     private Map<String, String> parameters;
 
-    public LoginRequest_stu(String userID, String userPassword, Response.Listener<String> listener) {
+    public LoginRequest(String ID, String Password, Response.Listener<String> listener) {
         super(Method.POST, URL, listener, null);
         parameters = new HashMap<>();
-        parameters.put("userID", userID);
-        parameters.put("userPassword", userPassword);
+        parameters.put("ID", ID);
+        parameters.put("Password", Password);
 
     }
 
